deda794fddba0950e0afdf7edee27d7bf5fec8e2
rvst312 <115166784+rvst312@users.noreply.github.com>
2023-05-01 22:09:08 +0200

    new

--- scripts.js
+++ scripts.js
@@ -1,8 +1,18 @@
-
+//Dinamic characters 
 const url = `https://rickandmortyapi.com/api/character`
 
 fetch(url)
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.results)
+        printData(data.results)
+    })
+    .catch(error => console.log(error))
+
+function printData(data) {
+    data.forEach(character => {
+        const { name, gender, species, status, location, image} = character
+        console.log(character)
+
+    })
+}
